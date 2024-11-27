ba85037757d6588c45c2472e252a65d642f0ebcc
Dwisans <wiwik1161@gmail.com>
2024-11-27 13:20:40 +0700

    Membuat Hero Section di src/index.html dan memperbarui src/output.css dan src/tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#047857",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
   },
   plugins: [],
-}
-
+};
